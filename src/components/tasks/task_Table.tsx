785c6e1cb276c0ae721ea64a5032e387c863fea2
Muhammadtawil <muhammad.tawil@hotmail.com>
2024-01-15 08:54:39 +0200

    EDIT TASK ASSIGN

--- src/components/tasks/task_Table.tsx
+++ src/components/tasks/task_Table.tsx
@@ -240,7 +240,7 @@ const [title,setTitle]=useState("")
     );
 
     const noTasks = filteredTasks.length === 0;
-    const admin = session?.UserRole === "ADMIN";
+    const admin = session?.UserRole === "ADMIN" ||session?.UserRole==="SUPER ADMIN";
     return (
 
         <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
